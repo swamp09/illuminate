7fa1d0199cb8f054c076f304daad50ef201f410d
swamp09 <shu.numata@esm.co.jp>
2019-01-17 00:43:31 +0900

    Fix typo

--- db/migrate/20190116153806_create_events.rb
+++ db/migrate/20190116153806_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :type, null: false
-      t.interger :actor_id, null: false
+      t.integer :actor_id, null: false
       t.datetime :timestamp, null: false
 
       t.timestamps
