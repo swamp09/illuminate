9428fc2558275f5018f3cbec8b7226b6b124078c
swamp09 <shu.numata@esm.co.jp>
2019-01-17 00:43:12 +0900

    Add not null constraint

--- db/migrate/20190116153124_create_actors.rb
+++ db/migrate/20190116153124_create_actors.rb
@@ -1,8 +1,8 @@
 class CreateActors < ActiveRecord::Migration[5.2]
   def change
     create_table :actors do |t|
-      t.string :name
-      t.string :icon_url
+      t.string :name, null: false
+      t.string :icon_url, null: false
 
       t.timestamps
     end
