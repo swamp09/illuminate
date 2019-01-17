c72c2af63a72958e29c4f18d59743bedc45215c2
swamp09 <shu.numata@esm.co.jp>
2019-01-18 00:21:47 +0900

    Refactor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,20 +1,16 @@
 class ActorsController < ApplicationController
   before_action :set_actor, only: :show
 
-  # GET /actors
-  # GET /actors.json
   def index
     @actors = Actor.all
   end
 
-  # GET /actors/1
-  # GET /actors/1.json
   def show
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_actor
-      @actor = Actor.find(params[:id])
-    end
+
+  def set_actor
+    @actor = Actor.find(params[:id])
+  end
 end
