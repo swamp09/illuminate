64f4d8114a1954acb8812647e2e1d96487b078ec
swamp09 <shu.numata@esm.co.jp>
2019-06-28 22:14:36 +0900

    Add role column to actors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_155046) do
+ActiveRecord::Schema.define(version: 2019_06_28_131329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_155046) do
     t.string "icon_url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", null: false
     t.index ["name"], name: "index_actors_on_name", unique: true
   end
 
